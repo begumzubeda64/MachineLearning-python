cd6bd9995539dc4e5d5639ecde1f3a47ffd87498
Zubeda Abbas <bzubeda64@gmail.com>
2021-07-28 19:52:00 +0530

    Machine learning python - linear regression

---
+++ main.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from  sklearn.metrics import mean_squared_error
+
+# (['data', 'target', 'frame', 'DESCR', 'feature_names', 'data_filename', 'target_filename'])
+diabetes = datasets.load_diabetes()
+
+#print(diabetes.keys())
+# (['data', 'target', 'frame', 'DESCR', 'feature_names', 'data_filename', 'target_filename'])
+# diabetes_X = diabetes.data[:, np.newaxis, 2] #One feature
+diabetes_X = diabetes.data
+diabetes_X_train = diabetes_X[:-30] #first 30
+diabetes_X_test = diabetes_X[-30:] #last 30
+
+diabetes_Y_train = diabetes.target[:-30]
+diabetes_Y_test = diabetes.target[-30:]
+
+model = linear_model.LinearRegression()
+model.fit(diabetes_X_train, diabetes_Y_train)
+
+diabetes_Y_predict = model.predict(diabetes_X_test)
+print("Mean squared error: ", mean_squared_error(diabetes_Y_test, diabetes_Y_predict))
+print("Weights: ", model.coef_)
+print("Intercept: ", model.intercept_)
+
+# one feature
+# plt.scatter(diabetes_X_test, diabetes_Y_test)
+# plt.plot(diabetes_X_test, diabetes_Y_predict)
+# plt.show()
+
+# Output:
+# Mean squared error:  3035.0601152912695
+# Weights:  [941.43097333]
+# Intercept:  153.39713623331698
+
